a0ea5b3ded0ba7779069e801ec10ff88e29ce5e3
Michal Klesinski <michkles@Michals-MacBook-Pro.local>
2016-11-28 21:08:43 +0000

    added second test and deposit method

--- account.rb
+++ account.rb
@@ -1,5 +1,12 @@
 class Account
-  def balance
-    0
+
+  attr_reader :balance
+  def initialize
+    @balance = 0
   end
-end    
+
+  def deposit(amount)
+    @balance = @balance + amount
+  end  
+
+end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,4 +5,10 @@ describe Account do
   it "should start with balance of 0" do
     expect(subject.balance).to eq 0
   end
+
+  describe  "#deposit" do
+    it "should increase the balance by given amount" do
+      expect{ subject.deposit(1000) }.to change{ subject.balance }.by 1000
+    end
+  end
 end
