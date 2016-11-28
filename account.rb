6d48799a456e5b9311e32ac3d3f9a1fcc34309f3
Michal Klesinski <michkles@Michals-MacBook-Pro.local>
2016-11-28 23:33:02 +0000

    added tests for withdraw and deposit methods

--- account.rb
+++ account.rb
@@ -1,12 +1,24 @@
 class Account
 
-  attr_reader :balance
+  attr_reader :balance, :transactions
   def initialize
     @balance = 0
+    @transactions = []
   end
 
-  def deposit(amount)
-    @balance = @balance + amount
-  end  
+  def deposit(date, amount)
+    @balance += amount
+    add_transactions(date, amount, type=:deposit)
+  end
+
+  def withdraw(date, amount)
+    @balance -= amount
+    add_transactions(date, amount, type=:credit)
+  end
+
+  private
+    def add_transactions(date, amount, type)
+      @transactions.push({ date: date, amount: amount, balance: balance, type: type})
+    end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -8,7 +8,24 @@ describe Account do
 
   describe  "#deposit" do
     it "should increase the balance by given amount" do
-      expect{ subject.deposit(1000) }.to change{ subject.balance }.by 1000
+      expect{ subject.deposit('01/01/2017', 1000) }.to change{ subject.balance }.by 1000
+    end
+
+    it "should be stored as deposit in transactions" do
+      subject.deposit('01/01/2017', 1000)
+      expect(subject.transactions[0]).to have_value(:deposit)
     end
   end
+
+  describe  "#withdraw" do
+    it "should decrease the balance by given amount" do
+      expect{ subject.withdraw('01/01/2017', 500) }.to change{ subject.balance }.by -500
+    end
+
+    it "should be stored as credit in transactions" do
+      subject.withdraw('01/01/2017', 500)
+      expect(subject.transactions[0]).to have_value(:credit)
+    end
+  end
+
 end
